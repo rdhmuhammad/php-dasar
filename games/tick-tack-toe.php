807b03d71ddfda9df7e032e68eaa64310ad274c0
rdhmuhammad <rdhmuhammad57@gmail.com>
2023-09-28 09:39:59 +0700

    feat: add button active animation

--- games/tick-tack-toe.php
+++ games/tick-tack-toe.php
@@ -26,6 +26,7 @@
             border-radius: 10%;
             display: flex;
             align-items: center;
+            border: none;
             justify-content: center;
             place-self: stretch;
             background-color: palevioletred;
@@ -38,9 +39,20 @@
                     to bottom,
                     #f897b6,
                     transparent 10%);
+            transition: 1s;
         }
 
         .container > button:hover {
+            background-color: #ff36a8;
+            background-image: radial-gradient(#ffa2d4, #ff36a8 90%);
+        }
+
+        .container > button:active {
+            box-shadow: 0 0 9px 3px rgba(255, 248, 120, 1);
+        }
+
+        .container > button {
+            transition: 0.05s;
             font-size: 50px;
             font-family: "Lato Light", serif;
             color: azure;
